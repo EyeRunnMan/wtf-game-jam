036c59ab355f2905ea4c5ffcd8eb9c572877f6c0
Guru <gurubaran1997@gmail.com>
2024-07-21 10:08:28 +0530

    Fixed errors in InputLineRenderer

--- Assets/Scripts/Components/InputLineRenderer.cs
+++ Assets/Scripts/Components/InputLineRenderer.cs
@@ -1,30 +1,33 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WTF.Common;
+
 namespace WTF.Common.InputSystem.Components
 {
     [RequireComponent(typeof(LineRenderer))]
     public class InputLineRenderer : MonoBehaviour
     {
-        IInputSystem inputSystem;
-        [SerializeField]
-        LineRenderer lineRenderer;
+        [SerializeField] private LineRenderer lineRenderer;
+
+        private IInputSystem inputSystem;
+        private List<Vector3> positions = new List<Vector3>();
+
         private void Start()
         {
-            WTF.Common.DependencySolver.TryGetInstance(out inputSystem);
+            DependencySolver.TryGetInstance(out inputSystem);
             inputSystem.OnSwipeStartEvent += OnInterActionStarted;
-            inputSystem.OnDuringSwipEvent += OnDuringInteraction;
+            inputSystem.OnDuringSwipeEvent += OnDuringInteraction;
             inputSystem.OnSwipeEventEnded += OnInteractionEnded;
-
         }
+
         private void OnDestroy()
         {
             inputSystem.OnSwipeStartEvent -= OnInterActionStarted;
-            inputSystem.OnDuringSwipEvent -= OnDuringInteraction;
+            inputSystem.OnDuringSwipeEvent -= OnDuringInteraction;
             inputSystem.OnSwipeEventEnded -= OnInteractionEnded;
         }
-        List<Vector3> positions = new List<Vector3>();
+
         private void OnInteractionEnded()
         {
             positions.Clear();
@@ -35,19 +38,17 @@ namespace WTF.Common.InputSystem.Components
 
         private void OnDuringInteraction(Vector2 vector)
         {
-            positions.Add(Camera.main.ScreenToWorldPoint(vector));
+            positions.Add(vector);
             lineRenderer.positionCount = positions.Count;
             lineRenderer.SetPositions(positions.ToArray());
         }
 
-        private void OnInterActionStarted()
+        private void OnInterActionStarted(Vector2 _)
         {
             positions.Clear();
 
             lineRenderer.positionCount = 0;
             lineRenderer.SetPositions(positions.ToArray());
         }
-
     }
-
 }
