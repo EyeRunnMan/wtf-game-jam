5ddbff4652f99357eb918704e2484cd5018f41a5
EyeRunnMan <46531095+EyeRunnMan@users.noreply.github.com>
2024-07-21 14:14:22 +0530

    fat: added game hud script

---
+++ Assets/Scripts/Controllers/GameHUDReferences.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameHUDReferences : MonoBehaviour
+{
+    public HUDPlayerProgressWidget leftProgressWidget;
+    public HUDPlayerProgressWidget rightProgressWidget;
+    public Button pauseButton;
+}
