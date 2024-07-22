126145749cf851c1f28fc9f8106ed58d7989046e
Guru <gurubaran1997@gmail.com>
2024-07-22 07:05:32 +0530

    Updated bot config values

--- Assets/Scripts/Constants/BotConfig.cs
+++ Assets/Scripts/Constants/BotConfig.cs
@@ -4,8 +4,8 @@ namespace WTF.Bot
 {
     public class BotConfig
     {
-        public static readonly Vector2 WaitBeforeMerge = new Vector2(15, 60);
-        public static readonly Vector2 WaitBeforeExplosion = new Vector2(5, 30);
+        public static readonly Vector2 WaitBeforeMerge = new Vector2(15, 35);
+        public static readonly Vector2 WaitBeforeExplosion = new Vector2(5, 15);
 
         public static readonly float[] WeightsForMerge = {30, 70, 90, 100};
     }
