5fdd20e09b9fde5946bb187f162f2fcbc55d3748
Satyam <69568251+Super-Satyam@users.noreply.github.com>
2024-07-22 07:41:54 +0530

    updated start enemies count

--- Assets/Scripts/Constants/LevelCreepsConfig.cs
+++ Assets/Scripts/Constants/LevelCreepsConfig.cs
@@ -2,8 +2,8 @@ namespace WTF.Configs
 {
     public class LevelCreepsConfig
     {
-        public static readonly int LevelStartPlayerCreepsCount = 5;
-        public static readonly int LevelStartEnemyCreepsCount = 5;
+        public static readonly int LevelStartPlayerCreepsCount = 3;
+        public static readonly int LevelStartEnemyCreepsCount = 6;
         public static readonly int TotalCreepsCount = 25;
 
         public static readonly int MaxCreepGrouping = 5;
